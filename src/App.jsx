82ba3c53351dab2ff79d38c6ceb34e81f7db3bb6
igorhnq <igorhenrique102@gmail.com>
2024-10-20 22:40:51 -0300

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate itaque dolore similique quia saepe, libero excepturi provident exercitationem autem laborum culpa aut iure nulla quidem ut fuga maiores magnam fugiat!"  
-      />
-      <Post 
-        author="Gabriel Buzzi" 
-        content="Um novo post muito legal"  
-      />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Diego Fernandes" 
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate itaque dolore similique quia saepe, libero excepturi provident exercitationem autem laborum culpa aut iure nulla quidem ut fuga maiores magnam fugiat!"  
+          />
+          <Post 
+            author="Gabriel Buzzi" 
+            content="Um novo post muito legal"  
+          />
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
