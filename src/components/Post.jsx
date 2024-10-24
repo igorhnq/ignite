c53aab71312d98a2a09bfda4ae7e4a654d6b510d
igorhnq <igorhenrique102@gmail.com>
2024-10-23 21:11:42 -0300

    refactor: replace img with avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/127193871?v=4" />
+                    <Avatar hasBorder src="https://github.com/ericopieri.png" />
                     <div className={styles.authorInfo}>
                         <strong>Igor Henrique</strong>
                         <span>Web Developer</span>
