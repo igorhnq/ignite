c54caf1af0be28443b7991051d9a714b3c7b1828
igorhnq <igorhenrique102@gmail.com>
2024-10-24 22:21:26 -0300

    feat: add post content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/igorhnq.png',
+      name: 'Igor Henrique',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/ericopieri.png',
+      name: 'Erico Pieri',
+      role: 'Student'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  }
+];
 
 export function App() {
   return (
@@ -14,15 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Diego Fernandes" 
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate itaque dolore similique quia saepe, libero excepturi provident exercitationem autem laborum culpa aut iure nulla quidem ut fuga maiores magnam fugiat!"  
-          />
-          <Post 
-            author="Gabriel Buzzi" 
-            content="Um novo post muito legal"  
-          />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
