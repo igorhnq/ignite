3b139eaaae445ed514d442cf8b5dbfa6abc296d9
igorhnq <igorhenrique102@gmail.com>
2024-10-28 23:36:08 -0300

    refactor: add typescript interface to post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,29 @@
 import { format, formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { ptBR } from 'date-fns/locale/pt-BR'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 
-export function Post({ author, publishedAt, content}) {
+interface Author {
+    name: string
+    role: string
+    avatarUrl: string
+}
+
+interface Content {
+    type: 'paragraph' | 'link'
+    content: string
+}
+
+interface PostProps {
+    author: Author
+    publishedAt: Date
+    content: Content[]
+}
+
+export function Post({ author, publishedAt, content}: PostProps) {
     const [comments, setComments]= useState([
         'Post muito banaca, hein?!'
     ])
@@ -22,25 +39,23 @@ export function Post({ author, publishedAt, content}) {
         addSuffix: true
     })
 
-    function handleCreateNewComment(event) {
+    function handleCreateNewComment(event: FormEvent) {
         event.preventDefault()
-
-        const newCommentText = event.target.comment.value
 
         setComments([...comments, newCommentText])
         setNewCommentText('')
     }
 
-    function handleNewCommentChange(event) {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
     }
 
-    function handleNewCommentInvalid(event) {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('Esse campo é obrigatório')
     }
 
-    function deleteComment(commentToDelete) {
+    function deleteComment(commentToDelete: String) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
             return comment !== commentToDelete
         })
